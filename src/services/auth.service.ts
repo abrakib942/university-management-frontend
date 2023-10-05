84dda78988ff2672f1ac69a5effd89b4b43ebd6e
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-10-06 00:59:30 +0600

    get from LS and decode

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,24 @@
-import { setToLocalStorage } from "@/utils/local-storage";
+import { authKey } from "@/constants/storagekey";
+import { decodedToken } from "@/utils/jwt";
+import { getFromLocalStorage, setToLocalStorage } from "@/utils/local-storage";
 
 export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
-  setToLocalStorage("accessToken", accessToken as string);
+  return setToLocalStorage("accessToken", accessToken as string);
+};
+
+export const getUserInfo = () => {
+  const authToken = getFromLocalStorage(authKey); // = "accessToken"
+
+  if (authToken) {
+    const decodedData = decodedToken(authToken);
+    return decodedData;
+  } else {
+    return "";
+  }
+};
+
+export const isLoggedIn = () => {
+  const authToken = getFromLocalStorage(authKey);
+
+  return !!authToken;
 };
