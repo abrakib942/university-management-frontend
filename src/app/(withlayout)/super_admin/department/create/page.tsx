c107b38155f7d2eb468c5caccdbc5e0888a4bcee
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-10-31 13:13:44 +0600

    custom table

--- src/app/(withlayout)/super_admin/department/create/page.tsx
+++ src/app/(withlayout)/super_admin/department/create/page.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+
+import CustomForm from "@/components/form/Form";
+import FormInput from "@/components/form/FormInput";
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { useAddDepartmentMutation } from "@/redux/api/departmentApi";
+import { Button, Col, Row, message } from "antd";
 
 const CreateDepartmentPage = () => {
-  return <div>Create Department</div>;
+  const [addDepartment] = useAddDepartmentMutation();
+
+  const onSubmit = async (data: any) => {
+    message.loading("Creating...");
+    try {
+      console.log(data);
+      await addDepartment(data);
+
+      message.success("Department added successfully");
+    } catch (err: any) {
+      console.error(err.message);
+      message.error(err.message);
+    }
+  };
+  const base = "super_admin";
+  return (
+    <div>
+      <UMBreadCrumb
+        items={[
+          { label: `${base}`, link: `/${base}` },
+          { label: "department", link: `/${base}/department` },
+        ]}
+      />
+      <h1>Create Department</h1>
+      <CustomForm submitHandler={onSubmit}>
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Col span={8} style={{ margin: "10px 0" }}>
+            <FormInput name="title" label="Title" />
+          </Col>
+        </Row>
+        <Button type="primary" htmlType="submit">
+          add
+        </Button>
+      </CustomForm>
+    </div>
+  );
 };
 
 export default CreateDepartmentPage;
